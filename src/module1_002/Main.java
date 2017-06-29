f1c2983abe4e768401c848551c0c77615f456916
euphoriaqn <euphoriaqn@gmail.com>
2017-06-29 22:44:52 +0300

    реализован "user data input"

--- src/module1_002/Main.java
+++ src/module1_002/Main.java
@@ -1,7 +1,21 @@
 package module1_002;
-
+import java.util.Scanner;
 /**
  * Created by Илья on 29.06.2017.
  */
 public class Main {
+    public static void main (String[] args){
+        ////////////////////////////////////////////////
+        ////////////////* USER DATA INPUT*//////////////
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String  name = sc.next();
+        System.out.println("Please enter city of your residence: ");
+        String  city = sc.next();
+        System.out.println("Please enter your age: ");
+        int  age = sc.nextInt();
+        System.out.println("Please enter your hobby: ");
+        String  hobby = sc.next();
+       
+    }
 }
