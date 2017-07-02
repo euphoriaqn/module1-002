0dbbe91efeca53a5d9a90876ac9fb26c2adba1c8
euphoriaqn <euphoriaqn@gmail.com>
2017-07-02 21:30:15 +0300

    метод Main разгружен от лишней информации

--- src/module1_002/Main.java
+++ src/module1_002/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Created by Илья on 29.06.2017.
  */
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         ////////////////////////////////////////////////
         ////////////////* USER DATA INPUT*//////////////
         Scanner sc = new Scanner(System.in);
@@ -22,41 +22,33 @@ public class Main {
             System.out.println("Enter variant of output: 1 - tabular, 2 - text, 3 - alternative");
             variant = sc.nextInt();
         } while (variant < 1 || variant > 3);
+        VarOfOutput(name, city, age, hobby, variant);
         ////////////////////////////////////////////////
+      }
+    //////////////*OUTPUT OF USER INPUT*//////////
+    //////////Tabular , Text , Alternative////////
+    public static void VarOfOutput(String name, String city, int age, String hobby, int variant) {
         switch (variant) {
             case 1:
-            tabularVar(name, city, age, hobby);
+                System.out.println("!!!Variant #1!!! ");
+                System.out.println("Name: " + name);
+                System.out.println("City: " + city);
+                System.out.println("Age: " + age);
+                System.out.println("Hobby: " + hobby);
                 break;
             case 2:
-            textVar(name, city, age, hobby);
+                System.out.println("!!!Variant #2!!! ");
+                System.out.println("A human named " + name + " lives in the city of " + city + ".");
+                System.out.println("This person is " + age + " years old and he/she loves " + hobby);
                 break;
             case 3:
-            alternatVar(name, city, age, hobby);
+                System.out.println("!!!Variant #3!!! ");
+                System.out.println(name + " - name");
+                System.out.println(city + " - city");
+                System.out.println(age + " - age");
+                System.out.println(hobby + " - hobby");
+                break;
         }
+
     }
-        //////////////*OUTPUT OF USER INPUT*//////////
-        ///Tabular variant
-        public static void tabularVar(String name, String city, int age, String hobby) {
-            System.out.println("!!!Variant #1!!! ");
-            System.out.println("Name: " + name);
-            System.out.println("City: " + city);
-            System.out.println("Age: " + age);
-            System.out.println("Hobby: " + hobby);
-        }
-        ///////////////////////////////////////////////
-        ///Text variant
-        public static void textVar(String name, String city, int age, String hobby) {
-            System.out.println("!!!Variant #2!!! ");
-            System.out.println("A human named " + name + " lives in the city of " + city + ".");
-            System.out.println("This person is " + age + " years old and he/she loves " + hobby);
-        }
-        ///////////////////////////////////////////////
-        ///Alternative variant
-        public static void alternatVar(String name, String city, int age, String hobby) {
-            System.out.println("!!!Variant #3!!! ");
-            System.out.println(name + " - name");
-            System.out.println(city + " - city");
-            System.out.println(age + " - age");
-            System.out.println(hobby + " - hobby");
-        }
-    }
+}
